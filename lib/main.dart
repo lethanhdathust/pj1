e8ea75e6fc26076531182d4f0681c1c3e04c0a35
Le Thanh Dat <Author identity unknown>
2022-11-28 21:19:33 +0700

    '1'

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import "dart:io";
 
 String takeMiddleName(String x) {
-List<String>name = []; 
-name = x.split(' ');
-  return name[name.length-1] ;
+  List<String> name = [];
+  name = x.split(' ');
+  return name[name.length - 1];
 }
 
 void main() {
   stdout.write('LE THI HOA - PROJECT1\n');
-  List<String> a = [
-    'le thanh Dat',
-    'cao Thị Hn',
-    'hoang Thị Hinh',
-    'le di An'
-  ];
+  List<String> a = ['le thanh Dat', 'cao Thị Hn', 'hoang Thị Hinh', 'le di An'];
   List<String> name = [];
   for (int i = 0; i < a.length; i++) {
     name.add(takeMiddleName(a[i]));
@@ -26,4 +21,5 @@ void main() {
     }
   }
   stdout.write(count);
+  var a = 1;
 }
